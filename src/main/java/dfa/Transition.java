46f3a7de2cf3fd565bd88ad84905b6f09a4866c0
sina <3inroydl@gmail.com>
2024-04-18 00:44:34 +0330

    add string-format field to Transition

--- src/main/java/dfa/Transition.java
+++ src/main/java/dfa/Transition.java
@@ -5,10 +5,17 @@ public class Transition {
     private State finalState;
     private Character character;
 
+    private String stringformat;
+
+    public String getStringformat() {
+        return stringformat;
+    }
+
     public Transition(State startState, State finalState, Character character) {
         this.startState = startState;
         this.finalState = finalState;
         this.character = character;
+        this.stringformat = startState.getName()+","+character.toString()+","+finalState.getName();
     }
 
     public State getFinalState() {
@@ -33,5 +40,12 @@ public class Transition {
 
     public void setFinalState(State finalState) {
         this.finalState = finalState;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.startState == ((Transition) obj).startState &&
+                this.startState == ((Transition) obj).startState &&
+                this.character == ((Transition) obj).character;
     }
 }
